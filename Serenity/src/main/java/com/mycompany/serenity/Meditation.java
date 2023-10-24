7625a2c3d7dac2d9c42895da08895fb4147cf3fd
joshblank007 <veerjv@farmingdale.edu>
2023-10-23 19:55:08 -0700

    put review page in correct folder

--- Serenity/src/main/java/com/mycompany/serenity/Meditation.java
+++ Serenity/src/main/java/com/mycompany/serenity/Meditation.java
@@ -1,8 +1,26 @@
 package com.mycompany.serenity;
+import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Hyperlink;
+import javafx.stage.Stage;
+import javafx.scene.layout.VBox;
+import java.io.IOException;
+import javafx.scene.Scene;
 
-public class Meditation {
+public class Meditation extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        VBox root = FXMLLoader.load(getClass().getResource("Meditation.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Testing Hyperlink");
+        stage.show();
+    }
+
+    public static void main(String[] args){
+        launch(args);
+    }
 
 }
