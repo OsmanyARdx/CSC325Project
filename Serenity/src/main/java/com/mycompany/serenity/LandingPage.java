571d5870a171f0261a7a42e346f68278cc5a344a
OsmanyARdx <osmarguet@gmail.com>
2023-10-25 20:45:52 -0400

    EmergencyResources class methods

--- Serenity/src/main/java/com/mycompany/serenity/LandingPage.java
+++ Serenity/src/main/java/com/mycompany/serenity/LandingPage.java
@@ -1,6 +1,7 @@
 package com.mycompany.serenity;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
@@ -8,6 +9,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class LandingPage {
 
@@ -34,4 +36,22 @@ public class LandingPage {
             System.out.println(e);
         }
     }
+
+    public void switchPage(ActionEvent event, String page) {
+        try {
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource(page)));
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+    @FXML
+    public void handleClickEmergencyResources(ActionEvent event) {
+        switchPage(event, "EmergencyResources.fxml");
+    }
+
+
 }
